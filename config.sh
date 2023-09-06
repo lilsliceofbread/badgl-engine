05589ffa0f890f878952770adce980990726d46d
lilsliceofbread <yeshumankar@gmail.com>
2023-09-06 16:44:19 +0800

    build system boilerplate

---
+++ config.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+cmake -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_EXAMPLES=OFF -S . -B bin/build
--- src/main.c
+++ src/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
 
-int main(int argc, char* argv[]) {
-    printf("test");
+int main(int argc, char* argv[])
+{
+    GLFWwindow* window;
+
+    if(!glfwInit())
+    {
+        fprintf(stderr, "ERR: failed to init GLFW");
+        return -1;
+    }    
+
+    window = glfwCreateWindow(1280, 720, "learnopengl", NULL, NULL);
+    if(!window)
+    {
+        fprintf(stderr, "ERR: failed to open window");
+        return -1;
+    }
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
